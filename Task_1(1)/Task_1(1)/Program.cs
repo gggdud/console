4d24f445f99df89e8a3aebf1c2e744e138464a37
gggdud <95432881+gggdud@users.noreply.github.com>
2021-12-02 19:12:33 +0300

    Add files via upload

---
+++ Task_1(1)/Task_1(1)/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace Task_1_1_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double x1, x2, x3, y1, y2, y3;
+            try 
+            {
+                Console.Write("Введите x1: ");
+                x1 = Double.Parse(Console.ReadLine());
+                Console.Write("Введите y1: ");
+                y1 = Double.Parse(Console.ReadLine());
+                Console.Write("Введите x2: ");
+                x2 = Double.Parse(Console.ReadLine());
+                Console.Write("Введите y2: ");
+                y2 = Double.Parse(Console.ReadLine());
+                Console.Write("Введите x3: ");
+                x3 = Double.Parse(Console.ReadLine());
+                Console.Write("Введите y3: ");
+                y3 = Double.Parse(Console.ReadLine());
+                Console.WriteLine("Площадь тругольника: {0}", Math.Abs(0.5 * ((x1 - x3) * (y2 - y3) - (x2 - x3) * (y1 - y3))));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
---
+++ Task_1(2)/Task_1(2)/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+
+namespace Task_1_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double A, dVal;
+            string val;
+            try
+            {
+                Console.Write("Введите число A: ");
+                A = Double.Parse(Console.ReadLine());
+                Console.Write("Введите двузначное число: ");
+                dVal = Double.Parse(Console.ReadLine());
+                val = dVal.ToString();
+                if (val.Length != 2) 
+                {
+                    throw new Exception();
+                }
+                if ((Convert.ToDouble(val[0].ToString()) + Convert.ToDouble(val[1].ToString())) % A == 0)
+                {
+                    Console.WriteLine($"Сумма цифр числа {dVal} кратна числу {A}");
+                } 
+                else
+                {
+                    Console.WriteLine($"Сумма цифр числа {dVal} не кратна числу {A}");
+                }
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("Введено не двузначное число");
+            }
+        }
+    }
+}
