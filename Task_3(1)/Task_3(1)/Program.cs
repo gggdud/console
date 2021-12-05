1b1400c6cc2295dd4bf75651e0037dfc07a8e240
gggdud <95432881+gggdud@users.noreply.github.com>
2021-12-05 14:44:08 +0300

    Add files via upload

---
+++ Task_3(1)/Task_3(1)/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace Task_3_1_
+{
+    class Program
+    {
+        static int f(int x)
+        {
+            string str = x.ToString();
+            if(str.Length==2)
+            {
+                str = str[1].ToString() + str[0].ToString();
+                return Convert.ToInt32(str);
+            }
+            else
+            {
+                return x;
+            }
+        }
+        static void Main(string[] args)
+        {
+            int x;
+            try
+            {
+                Console.Write("Введите х: ");
+                x = int.Parse(Console.ReadLine());
+                Console.WriteLine("Итог: {0}", f(x));
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Вы ввели что-то не то");
+            }
+        }
+    }
+}
---
+++ Task_3(2_3)/Task_3(2_3)/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+
+namespace Task_3_2_3_
+{    
+    class Program
+    {
+        static void f(double a, double b, double h)
+        {
+            for (double x = a; x <= b; x += h)
+            {
+                if (x<=5)
+                {
+                    Console.WriteLine($"f({x}) = {x*x+5}");
+                }
+                else if (x<20)
+                {
+                    Console.WriteLine($"f({x}) = 0");
+                }
+                else
+                {
+                    Console.WriteLine($"f({x}) = 1");
+                }
+            }
+        }
+        static void f(double x, out double y)
+        {
+            if (x <= 5)
+            {
+                y = x * x + 5;
+            }
+            else if (x < 20)
+            {
+                y = 0;
+            }
+            else
+            {
+                y = 1;
+            }
+        }
+        static void Main(string[] args)
+        {
+            double a, b, h;
+            try
+            {
+                Console.Write("Введите a: ");
+                a = Double.Parse(Console.ReadLine());
+                Console.Write("Введите b: ");
+                b = Double.Parse(Console.ReadLine());
+                Console.Write("Введите h: ");
+                h = Double.Parse(Console.ReadLine());
+                f(a, b, h);
+
+                Console.Write("\nЧасть 3\n");
+                for (double x = a; x <= b; x += h)
+                {
+                    double y;
+                    f(x, out y);
+                    Console.Write("{0} ", y);
+                }
+            }
+            catch(System.FormatException)
+            {
+                Console.WriteLine("Введено что-то не то");
+            }
+        }
+    }
+}
